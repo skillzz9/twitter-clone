ee60ca7884ce1a932ce021d9b681daba2bb04dff
skillzz9 <hpos7501@uni.sydney.edu.au>
2024-07-11 12:52:31 +1000

    sign up modal completed

--- components/Modals/SignUpModal.js
+++ components/Modals/SignUpModal.js
@@ -12,8 +12,27 @@ export default function SignUpModal(){
         <button onClick={() => dispatch(openSignupModal())} className="bg-white border text-black w-[160px] h-[40px] rounded-full hover:bg-[#cbd2d7]"> Sign up</button>
 
         <Modal onClick={() => dispatch(closeSignupModal())} open={isOpen} className="flex justify-center items-center">
-            <div className="w-[400px] h-[200px] bg-white">
-                Sign up today
+            <div className="w-[90%] h-[400px] bg-black text-white md:w-[550px] md:h-[600px] border border-gray-700 rounded-lg">
+                <div className="flex flex-col items-center mt-10">
+                    <button className=" bg-white w-[90%] h-10 rounded-lg text-black font-bold text-lg">
+                        Sign In as Guest
+                    </button>
+                    <p className="text-white p-3 text-bold text-sm font-bold">
+                        or
+                    </p>
+                </div>
+                <h1 className="text-white p-3 text-bold text-4xl font-bold ml-5">
+                        Create your account
+                </h1>
+                <div className="flex flex-col items-center">
+                <input placeholder="Full Name" className="w-[90%] mt-8 h-10 rounded-md bg-transparent border border-gray-700 p-6" type={"name"}></input>
+                <input placeholder="Email" className="w-[90%] mt-8 h-10 rounded-md bg-transparent border border-gray-700 p-6" type={"name"}></input>
+                <input placeholder="Password" className="w-[90%] mt-8 h-10 rounded-md bg-transparent border border-gray-700 p-6" type={"name"}></input>
+                
+                <button className="mt-9 bg-white w-[90%] h-10 rounded-lg text-black font-bold text-lg">
+                      Create Account
+                </button>
+                </div>
             </div>
 
         </Modal>
