9a1827f426d9d7d1224a69d4ca3bfaf1c4fcdaed
skillzz9 <hpos7501@uni.sydney.edu.au>
2024-07-18 10:56:26 +1000

    auto format finally set up as well as comments showing up from firebase

---
+++ components/CommentBox.js
@@ -0,0 +1,51 @@
+import Moment from 'react-moment';
+
+export default function CommentBox({ user, tweetData }) {
+  return (
+    <>
+      <div className="flex justify-between items-center border-b border-gray-700 p-2">
+        <div className="flex justify-center items-center p-1 space-x-2">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src={user.photoUrl}
+          ></img>
+          <h1 className="text-2xl text-gray-500">Tweet your reply</h1>
+        </div>
+
+        <button
+          className="bg-[#1d9bf0] rounded-full px-4 py-1.5 disabled:opacity-50"
+          disabled={true}
+        >
+          Tweet
+        </button>
+      </div>
+      <Comments tweetData={tweetData} />
+    </>
+  );
+}
+
+export function Comments({ tweetData }) {
+  return (
+    <>
+      {tweetData.comments?.map((comment) => (
+        <div className="border-b border-gray-700">
+          <div className="flex space-x-3 p-3 ">
+            <img
+              className="w-11 h-11 rounded-full object-cover"
+              src={comment.photoUrl}
+            />
+
+            <div>
+              <div className="text-gray-500 flex items-center space-x-2 mb-1">
+                <h1 className="text-white font-bold">{comment.name}</h1>
+                <span>@{comment.usename}</span>
+                <div className="w-1 h-1 bg-gray-500 rounded-full"></div>
+              </div>
+              <span className="text-2xl">{comment.comment}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
--- pages/[id].js
+++ pages/[id].js
@@ -1,32 +1,74 @@
-import PostsFeed from "@/components/PostsFeed";
-import Sidebar from "@/components/Sidebar";
-import Trending from "@/components/Trending";
-import Tweet from "@/components/Tweet";
-import { db } from "@/firebase";
-import { ArrowLeftIcon } from "@heroicons/react/outline";
-import { doc, getDoc } from "firebase/firestore";
+import CommentBox, { Comments } from '@/components/CommentBox';
+import PostsFeed from '@/components/PostsFeed';
+import Sidebar from '@/components/Sidebar';
+import Trending from '@/components/Trending';
+import Tweet from '@/components/Tweet';
+import { db } from '@/firebase';
+import { ArrowLeftIcon } from '@heroicons/react/outline';
+import { doc, getDoc } from 'firebase/firestore';
+import Link from 'next/link';
+import Moment from 'react-moment';
+import { useSelector } from 'react-redux';
 
-export async function getServerSideProps(context){
-    const id = context.query.id
-    const docRef = doc(db, "posts", id)
-    const docSnap = await getDoc(docRef)
+export async function getServerSideProps(context) {
+  const id = context.query.id;
+  const docRef = doc(db, 'posts', id);
+  const docSnap = await getDoc(docRef);
+  const data = docSnap.data();
+  const formattedData = {
+    username: data?.userName,
+    name: data?.name,
+    photoUrl: data?.photoUrl,
+    text: data?.tweet,
+    comments: data.comments || null,
+    timestamp: JSON.stringify(data?.timestamp.toDate()),
+  };
+
+  return {
+    props: {
+      tweetData: formattedData,
+    },
+  };
 }
 
-export default function CommentsPage(){
-    return (
-        <div className="bg-black">
-            <div className='flex bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto'>
-                <Sidebar />
-                <div className="sm:ml-16 xl:ml-96 max-w-2xl flex-grow border-x border-gray-700">
-                    <div className="flex space-x-1 border-b border-gray-700">
-                    <ArrowLeftIcon className="w-7"/>
-                    <h1 className="px-3 py-2 text-lg sm:text-xl font-bold sticky top-0 z-0">Tweet</h1> 
-                    </div>
-    
-                <Tweet/>
+export default function CommentsPage({ tweetData }) {
+  const user = useSelector((state) => state.user);
+  return (
+    <div className="bg-black">
+      <div className="flex bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto">
+        <Sidebar />
+        <div className="sm:ml-16 xl:ml-96 max-w-2xl flex-grow border-x border-gray-700">
+          <div className="flex space-x-1 border-b border-gray-700">
+            <Link href={'/'}>
+              <ArrowLeftIcon className="w-7 mt-2 cursor-pointer" />
+            </Link>
+            <h1 className="px-3 py-2 text-lg sm:text-xl font-bold sticky top-0 z-0">
+              Tweet
+            </h1>
+          </div>
+
+          <div className="border-b border-gray-700">
+            <div className="flex space-x-3 p-3 ">
+              <img
+                className="w-11 h-11 rounded-full object-cover"
+                src={tweetData.photoUrl}
+              />
+
+              <div>
+                <div className="text-gray-500 flex items-center space-x-2 mb-1">
+                  <h1 className="text-white font-bold">{tweetData.name}</h1>
+                  <span>@{tweetData.usename}</span>
+                  <div className="w-1 h-1 bg-gray-500 rounded-full"></div>
+                  <Moment fromNow>{JSON.parse(tweetData.timestamp)}</Moment>
                 </div>
+                <span className="text-2xl">{tweetData.text}</span>
+              </div>
             </div>
-            <Trending />
+          </div>
+
+          <CommentBox user={user} tweetData={tweetData} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
